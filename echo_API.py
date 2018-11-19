d18bbd693d5b39fec7f7a3abb3b0d96685865961
alizaisaacs <ai2376@barnard.edu>
2018-11-19 15:09:03 -0500

    Add API for echo

---
+++ echo_API.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, abort
+import uuid, json, os, sys
+import threading
+from threading import Thread
+from echo_transmitter import main
+from echo_transmitter import turn_off, turn_on
+
+trx = None
+config = "/sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0032/all_leds"
+start_daemon = "/usr/local/bin/ledd &"
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = TRUE
+	
+@app.route('/api/transmit/<uuid>', methods=['POST'])
+def transmit(uuid):
+	global trx
+	
+	# Kill daemon process
+	os.system("if (fuser %s); then kill -9 $(fuser %s); fi" % (config, config))
+
+	# Extract FPS and start transmission using that value
+	data = request.get_json()
+	fps = data['FPS']
+	
+	if trx is None:
+		turn_on()
+		trx = threading.Thread(target = main, args = (fps,))
+		trx.start()
+	else:
+		return abort(400)
+	# Return session ID
+        return uuid
+
+@app.route('/api/off/<uuid>', methods=['POST'])
+def off(uuid):
+	global start_daemon
+	global trx
+	
+	# Terminate transmission and return to original state
+	if trx is not None:
+		turn_off()
+		trx.join()
+		os.system(start_daemon) 
+		return uuid
+	else:
+		return abort(400)
+	# end thread
+
+app.run()
