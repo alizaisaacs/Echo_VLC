06a86ec0dd1d47d60f346ffe0f1804c50a2c0ec0
Aliza Isaacs <39531144+alizaisaacs@users.noreply.github.com>
2018-12-03 15:22:18 -0500

    Update runapi.sh

--- runapi.sh
+++ runapi.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-. venv/bin/activate
-pip install -i https://pypi.python.org/simple/ flask
+
 iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
-python echo_API.py
+/home/root/venv/bin/python echo_API.py
