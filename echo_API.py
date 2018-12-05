442ae2cbcf62ff3be1951d42001597a9270d992e
Aliza Isaacs <39531144+alizaisaacs@users.noreply.github.com>
2018-12-05 14:00:29 -0500

    Update echo_API.py

--- echo_API.py
+++ echo_API.py
@@ -16,6 +16,12 @@ app = flask.Flask(__name__)
 def transmit(uuid):
 	global trx
 	
+	# Create SSL Key
+	os.chdir("/home/root")
+	os.system("mount -o remount rw /")
+	os.system("cat input.txt | openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem")
+	os.system("openssl x509 -in certificate.pem -noout -sha256 -fingerprint > fingerprint.txt")
+
 	# Kill daemon process
 	os.system("if (fuser %s); then kill -9 $(fuser %s); fi" % (config, config))
 
