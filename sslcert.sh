1d0e0b429875332931d35664b986f49d492a580e
Aliza Isaacs <39531144+alizaisaacs@users.noreply.github.com>
2018-12-05 13:34:49 -0500

    Update sslcert.sh

--- sslcert.sh
+++ sslcert.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+mount -o remount rw /
 cat "input.txt" | openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
-openssl x509 -in certificate.pem -noout -sha256 -fingerprint
+openssl x509 -in certificate.pem -noout -sha256 -fingerprint > fingerprint.txt
